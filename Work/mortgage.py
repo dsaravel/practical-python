eec903430f35558b8ddb01a8647d1f77529561ef
Doug Abreu <dsaravel@gmail.com>
2023-04-01 14:40:24 +1000

    calculating when adding extra 1000 per month

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,10 +4,19 @@
 
 principal = 500000.0
 rate = 0.05
-payment = 2684.11
+initial_monthly_payment = 2684.11
+monthly_payment = initial_monthly_payment
 total_paid = 0.0
+extra_payment_month = 12
 
 while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
-    total_paid = total_paid + payment
+    if extra_payment_month > 0:
+        extra_payment_month -= 1
+        monthly_payment = initial_monthly_payment + 1000
+    else:
+       monthly_payment = initial_monthly_payment
+
+    principal = principal * (1 + rate / 12) - monthly_payment
+    total_paid = total_paid + monthly_payment
+    print('monthly_payment:',monthly_payment)
 print("Total paid:",round(total_paid,2))
